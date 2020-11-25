7bae5baa4a13bf6eacbd63dfee8f2e09b87776d9
Marìa Carolina Gòmez <mayravargase01@gmail.com>
2020-11-24 20:43:07 -0500

    Js_tipo_de_operadores_24_11_2020_2

---
+++ main.js
@@ -0,0 +1,29 @@
+// let numero_1=5;
+// let numero_2=15;
+
+// numero_1+=2;
+// numero_1+=2;
+// numero_1+=2;
+
+// console.log(numero_1);
+
+
+// let numero_1=5;
+// let numero_2=15;
+// numero_1--;
+// numero_1-=numero_2;
+
+// console.log(numero_1);
+
+// let numero_1=5;
+// let numero_2=15;
+// numero_1 **=numero_2;
+// console.log(numero_1);
+
+// let numero_1=2;
+// let numero_2=15;
+// if(numero_1%=numero_2){
+//     console.log("numero impar");
+// }
+
+
